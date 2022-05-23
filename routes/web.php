ec7af68bbc424418327acd1062780e1ef92ac0e5
ahmadrobit <ahmadrobit@yahoo.co.id>
2022-05-23 09:27:37 +0700

    ikut tutorial di youtube

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 //    return view('welcome');
-    return view('beranda');
+    return view('home');
 });
+Route::get('/about', function () {
+        return view('about');
+});
+Route::get('/posts', function () {
+    return view('posts');
+});
+
 Route::redirect('/pusat', '/bola');
 
